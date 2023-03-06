670eff0837afe0761ff84ff2a50d683e94d14e1e
kdb <dabin6743@gmail.com>
2023-03-06 13:51:28 +0900

    init

---
+++ server/app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express()
+
+//build 폴더는 마음대로 꺼내가도 된다
+app.use(express.static('build'))
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/build/index.html')
+})
+
+app.listen(3000,()=>{
+    console.log('server is running!')
+});
+
